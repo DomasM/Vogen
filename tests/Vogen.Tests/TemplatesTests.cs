d12e50e2aa14e8f587022d8a1fe9b1eb0f2521a2
Steve Dunn <steve@dunnhq.com>
2023-01-28 18:39:01 +0000

    Fixed tests

--- tests/Vogen.Tests/TemplatesTests.cs
+++ tests/Vogen.Tests/TemplatesTests.cs
@@ -9,7 +9,7 @@ namespace Vogen.Tests;
 public class TemplatesTests
 {
 
-    private class Types : IEnumerable<object[]>
+    private class SpecificTypes : IEnumerable<object[]>
     {
         private readonly Type[] _types = new[]
         {
@@ -37,14 +37,43 @@ public class TemplatesTests
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
+
+    private class Anyypes : IEnumerable<object[]>
+    {
+        private readonly Type[] _types = new[]
+        {
+            typeof(bool), typeof(byte), typeof(char), typeof(DateOnly), typeof(DateTime), typeof(DateTimeOffset), typeof(decimal),
+            typeof(double), typeof(float), typeof(Guid), typeof(System.Int32), typeof(long), typeof(short), typeof(string),
+            typeof(TimeOnly)
+        };
+
+        private readonly string[] _technologies = new[]
+        {
+            "DapperTypeHandler", "EfCoreValueConverter", "LinqToDbValueConverter", "NewtonsoftJsonConverterReferenceType", "NewtonsoftJsonConverterValueType", "SystemTextJsonConverter",
+            "TypeConverter"
+        };
+        
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            foreach (var eachType in _types)
+            {
+                foreach (var eachTech in _technologies)
+                {
+                    yield return new object[] { eachType, eachTech };
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
     
     [Theory]
-    [ClassData(typeof(Types))]
-    public void CanGetItemFromTypeName(Type type, string tech) => Templates.TryGetForSpecificType(type, tech).Should().NotBeNull();
+    [ClassData(typeof(SpecificTypes))]
+    public void CanGetItemFromSpecificTypeName(Type type, string tech) => Templates.TryGetForSpecificType(type, tech).Should().NotBeNull();
 
     [Theory]
-    [ClassData(typeof(Types))]
+    [ClassData(typeof(Anyypes))]
 #pragma warning disable xUnit1026
-    public void CanGetItemForAnyType(Type unused, string tech) => Templates.GetForAnyType(tech).Should().NotBeNull();
+    public void CanGetItemForAnyOtherType(Type unused, string tech) => Templates.GetForAnyType(tech).Should().NotBeNull();
 #pragma warning restore xUnit1026
 }
